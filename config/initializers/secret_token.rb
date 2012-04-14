311d9ce6af7bbba06971b57ce99c8ce6750c0982
antonio <ajm.rive@gmail.com>
2012-04-14 13:55:12 +0100

    primera subida falso facebook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FakeFacebook::Application.config.secret_token = '9db4d8c1c3918fa94a799950a133364b42ae30e965e08e2feba44eaaa044cfe9e43205a68c9272e9346c0fe18ee7687cb97ba6ad22167dd7210e5cd0f7a1eca2'
