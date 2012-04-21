ad09bb307b2d7b207190ea00591fd4450762e86e
antonio <ajm.rive@gmail.com>
2012-04-21 13:46:41 +0100

    subiendo3,usuarios funcionando

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
-  attr_accessible :body, :user
+  attr_accessible :body, :user_id
+  
+  delegate :name, :to => :user, :prefix => true, :allow_nil => true
 end
